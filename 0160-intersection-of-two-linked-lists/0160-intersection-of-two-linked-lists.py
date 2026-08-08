11503f43f20844264d17a3d399054301283f8398
Tulsi Kumar Yadav <tk6377054@gmail.com>
2026-08-08 23:35:42 +0530

    Time: 274 ms (22.3%), Space: 66.6 MB (99.1%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -14,17 +14,20 @@ class Solution(object):
         p1 = headA
         p2 = headB
 
-        while(p1!= p2):
 
-            if p1:
+        # while both pointers are not equal  , traverse this..
+        while p1 != p2:
+
+            # when both pointers are not none..move to next.. and then any of the pointers becomes None
+            # Shift it to the next head, so that each of them have fair chances to move and covers equal 
+            # distance..
+            if p1 is not None:
                 p1 = p1.next
-
             else:
                 p1 = headB
 
-            if p2:
-                p2 = p2.next
-
+            if p2 is not None:
+                p2 = p2.next 
             else:
                 p2 = headA
 
